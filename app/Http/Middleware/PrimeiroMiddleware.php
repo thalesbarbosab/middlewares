5b87d882ce47a00eff2b621a2be761c3ce303393
Thales Bento <thales@dreamsti.com.br>
2019-12-12 21:12:29 -0300

    versao1

---
+++ app/Http/Middleware/PrimeiroMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Log;
+
+class PrimeiroMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Log::debug('Passou pelo Middleware 1');
+        //return response('não permite o proximo passo');
+        return $next($request);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<h3>Lista de Usuários</h3>
+<ul>
+    <li>João</li>
+    <li>Maria</li>
+    <li>José</li>
+    <li>Marques</li>
+</ul>
